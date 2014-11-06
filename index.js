08902373e21ee6c969dde9adc79984e4621d2b60
achingbrain <alex@achingbrain.net>
2014-11-06 08:40:01 +0000

    crypto utils and tests

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
   FileSystem: require('./lib/FileSystem'),
-  ProcessFactory: require('./lib/ProcessFactory')
+  ProcessFactory: require('./lib/ProcessFactory'),
+  Crypto: require('./lib/Crypto')
 }
